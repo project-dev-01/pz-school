8c881caef8e8bc0ff0105d227fbdc2aa1478dc59
karthiksure31 <karthiksure31@gmail.com>
2022-09-23 14:26:17 +0530

    exam results by teacher role

--- resources/views/teacher/exam_results/byclass.blade.php
+++ resources/views/teacher/exam_results/byclass.blade.php
@@ -34,7 +34,7 @@
                                     <label for="changeClassName">Standard<span class="text-danger">*</span></label>
                                     <select id="changeClassName" class="form-control" name="class_id">
                                         <option value="">Select Class</option>
-                                        <option value="All">All</option>
+                                        <!-- <option value="All">All</option> -->
                                         @forelse ($classnames as $class)
 
                                         <option value="{{ $class['class_id'] }}">{{ $class['class_name'] }}</option>
@@ -43,10 +43,10 @@
                                     </select>
                                 </div>
                             </div>
-                            <div class="col-md-3" id="section_drp_div">
+                            <div class="col-md-3">
                                 <div class="form-group">
-                                    <label for="sectionID" id="lblsectionId">Subject Name<span class="text-danger">*</span></label>
-                                    <select id="sectionID" class="form-control" name="section_id">
+                                    <label for="subjectID" id="lblsectionId">Subject Name<span class="text-danger">*</span></label>
+                                    <select id="subjectID" class="form-control" name="subject_id">
                                         <option value="">Select Subject</option>
                                     </select>
                                 </div>
@@ -75,7 +75,6 @@
     </div>
     <!-- end row -->
 
-
     <div class="row" style="display: none;" id="byclass_bodycontent">
         <div class="col-xl-12">
             <div class="card">
@@ -91,55 +90,15 @@
                         <input type="hidden" name="class_id" id="listModeClassID">
                         <input type="hidden" name="section_id" id="listModeSectionID">
                         <input type="hidden" name="exam_id" id="listModeexamID">
-                        <input type="hidden" name="fullmark" id="fullmark">
-                        <input type="hidden" name="passmark" id="passmark">
                         <div class="col-sm-12">
-                            <div class="card-body">
+                            <div id="byclassTableAppend">
 
-                                <!-- <div id="byclassTableAppend">
-
-                                </div> -->
-                                <div class="table-responsive">
-                                    <table id="tblbycls" class="table w-100 nowrap">
-                                        <thead>
-                                            <tr>
-                                                <th class="align-top" rowspan="2">S.no.</th>
-                                                <th class="align-top" rowspan="2">Class</th>
-                                                <th class="align-top th-sm - 6 rem" rowspan="2">Tot. Students</th>
-                                                <th class="align-top" rowspan="2">Absent</th>
-                                                <th class="align-top" rowspan="2">Present</th>
-                                                <th class="align-top" rowspan="2">Class Teacher Name</th>
-                                                @forelse ($allGrades as $val)
-                                                <th class="text-center" data-id="{{$val['id']}}">{{ $val['grade'] }}</th>
-                                                @empty
-                                                <th>0</th>
-                                                @endforelse
-
-                                                <th class="align-middle" rowspan="2">PASS</th>
-                                                <th class="align-middle" rowspan="2">G</th>
-                                                <th class="align-middle" rowspan="2">Avg. grade of subject</th>
-                                                <th class="align-middle" rowspan="2">%</th>
-                                            </tr>
-                                            <tr>
-                                                @forelse ($allGrades as $val)
-                                                <td class="text-center">%</td>
-                                                @empty
-                                                <th>0</th>
-                                                @endforelse
-                                            </tr>
-                                        </thead>
-                                        <tbody id="byclassTableAppend">
-
-                                        </tbody>
-                                    </table>
-                                </div><br>
-                                <div class="form-group text-right m-b-0">
-                                    <button class="btn btn-primary-bl waves-effect waves-light" type="submit">
-                                        Download
-                                    </button>
+                            </div>
+                            <div class="col-md-12">
+                                <div class="clearfix mt-4">
+                                    <button type="button" class="btn btn-primary-bl waves-effect waves-light float-right exportToExcel">Download</button>
                                 </div>
-
-                            </div> <!-- end col-->
+                            </div>
                         </div>
                         <!-- end row-->
 
@@ -150,47 +109,28 @@
         </div>
 
     </div> <!-- container -->
-    <div class="row" id="byclass_analysis">
+    <!-- <div class="row" id="byclass_analysis">
         <div class="col-lg-12">
             <div class="card">
-                <ul class="nav nav-tabs">
-                    <li class="nav-item">
-                        <h4 class="navv">
-                            Class Analysis
-                            <h4>
-                    </li>
-                </ul><br>
                 <div class="card-body">
+                    <h4 class="header-title">Class Analysis</h4>
+
                     <div class="mt-4 chartjs-chart">
                         <canvas id="radar-chart-test-byclass" height="350" data-colors="#39afd1,#a17fe0"></canvas>
-                        <!-- <canvas id="marksChart" height="350" data-colors="#39afd1,#a17fe0"></canvas> -->
                     </div>
-                </div> <!-- end card-body-->
-            </div> <!-- end card-->
-        </div> <!-- end col -->
-    </div>
+                </div>
+            </div>
+        </div>
+    </div> -->
     @endsection
     @section('scripts')
+    <script src="{{ asset('public/js/dist/jquery.table2excel.js') }}"></script>
+
     <script>
-        var sectionByClass = "{{ route('teacher.section_by_class') }}";
-        var teacherSubjectUrl = "{{ config('constants.api.teacher_subject') }}";
-        var examsList = "{{ config('constants.api.get_testresult_exams') }}";
-
-        var examsByclassandsection = "{{ config('constants.api.exam_by_classSection') }}";
         var examsByclassandsubject = "{{ config('constants.api.exam_by_classSubject') }}";
-
         var getbyClass = "{{ config('constants.api.tot_grade_calcu_byclass') }}";
-        var getbySubjectnames = "{{ config('constants.api.subject_by_class') }}";
-        var getbyClass_thead = "{{ config('constants.api.tot_grade_master') }}";
-        var Allexams = "{{ config('constants.api.all_exams_list') }}";
-        var getbyClassAllstd = "{{ config('constants.api.all_std_list') }}";
-        var getbysubjectnamesall = "{{ config('constants.api.class_assign_list') }}";
-
-        //
-        var getbySubject = "{{ config('constants.api.tot_grade_calcu_bySubject') }}";
-
-        var getbySubjectAllstd = "{{ config('constants.api.all_bysubject_list') }}";
-        //
+        var getbySubjectnames = "{{ config('constants.api.exam_results_get_subject_by_class') }}";
+        var teacher_id = "{{ Session::get('ref_user_id') }}";
         // default image test
         var defaultImg = "{{ asset('public/images/users/default.jpg') }}";
     </script>
--- resources/views/teacher/exam_results/bysubject.blade.php
+++ resources/views/teacher/exam_results/bysubject.blade.php
@@ -34,7 +34,7 @@
                                     <label for="changeClassName">Standard<span class="text-danger">*</span></label>
                                     <select id="changeClassName" class="form-control" name="class_id">
                                         <option value="">Select Class</option>
-                                        <option value="All">All</option>
+                                        <!-- <option value="All">All</option> -->
                                         @forelse ($classnames as $class)
 
                                         <option value="{{ $class['class_id'] }}">{{ $class['class_name'] }}</option>
@@ -89,82 +89,54 @@
                 <div class="card-body">
                     <div class="row">
                         <div class="col-sm-12">
-                            <div class="card-body">
-                                <div class="table-responsive">
-                                    <table id="tblbycls" class="table w-100 nowrap table-bordered table-striped">
-                                        <thead>
-                                            <tr>
-                                                <th class="align-top" rowspan="2">S.no.</th>
-                                                <th class="align-top" rowspan="2">Standard</th>
-                                                <th class="align-top" rowspan="2">Class</th>
-                                                <th class="align-top" rowspan="2">Subject Name</th>
-                                                <th class="align-top th-sm - 6 rem" rowspan="2">Tot. Students</th>
-                                                <th class="align-top" rowspan="2">Absent</th>
-                                                <th class="align-top" rowspan="2">Present</th>
-                                                <th class="align-top" rowspan="2">Class Teacher Name</th>
-                                                @forelse ($allGrades as $val)
-                                                <th class="text-center" data-id="{{$val['id']}}">{{ $val['grade'] }}</th>
-                                                @empty
-                                                <th>0</th>
-                                                @endforelse
-                                                <th class="align-middle" rowspan="2">PASS</th>
-                                                <th class="align-middle" rowspan="2">G</th>
-                                                <th class="align-middle" rowspan="2">Avg. grade of subject</th>
-                                                <th class="align-middle" rowspan="2">%</th>
-                                            </tr>
-                                            <tr>
-                                                @forelse ($allGrades as $val)
-                                                <td class="text-center">%</td>
-                                                @empty
-                                                <th>0</th>
-                                                @endforelse
-                                            </tr>
-                                        </thead>
-                                        <tbody id="bysubjectTableAppend">
+                            <div id="bysubjectTableAppend">
+                            </div>
+                            <!-- <div id="btnAppend">
+                            </div> -->
 
-                                        </tbody>
-                                    </table>
-                                </div> <!-- end table-responsive-->
-                            </div> <!-- end card-box -->
-                        </div> <!-- end col-->
-                    </div>
-                    <!-- end row-->
+                            <div class="col-md-12">
+                                <div class="clearfix mt-4">
+                                    <button type="button" class="btn btn-primary-bl waves-effect waves-light float-right exportToExcel">Download</button>
+                                </div>
+                            </div>
 
-                </div> <!-- end card-body -->
+                        </div>
+                        <!-- end row-->
+
+                    </div> <!-- end card-body -->
+                </div> <!-- end card-->
             </div> <!-- end card-->
         </div> <!-- end col -->
 
     </div>
-    <div class="row" id="bysubject_analysis">
+    <!-- <div class="row" id="bysubject_analysis">
         <div class="col-lg-12">
             <div class="card">
-                <ul class="nav nav-tabs">
-                    <li class="nav-item">
-                        <h4 class="navv">
-                            Subject Analysis
-                            <h4>
-                    </li>
-                </ul><br>
                 <div class="card-body">
+                    <h4 class="header-title">Subject Analysis</h4>
+
                     <div class="mt-4 chartjs-chart">
                         <canvas id="radar-chart-test-bystudentmarks" height="350" data-colors="#39afd1,#a17fe0"></canvas>
-                        <!-- <canvas id="marksChart" height="350" data-colors="#39afd1,#a17fe0"></canvas> -->
                     </div>
-                </div> <!-- end card-body-->
-            </div> <!-- end card-->
-        </div> <!-- end col -->
-    </div>
+                </div>
+            </div>
+        </div>
+    </div> -->
 </div> <!-- container -->
 
 @endsection
 @section('scripts')
+<script src="{{ asset('public/js/dist/jquery.table2excel.js') }}"></script>
+
 <script>
-    var sectionByClass = "{{ route('teacher.section_by_class') }}";
+    var sectionByClass = "{{ config('constants.api.exam_results_get_class_by_section') }}";
+
     var examsByclassandsection = "{{ config('constants.api.exam_by_classSection') }}";
     var getbySubject = "{{ config('constants.api.tot_grade_calcu_bySubject') }}";
     var Allexams = "{{ config('constants.api.all_exams_list') }}";
     var getbySubjectAllstd = "{{ config('constants.api.all_bysubject_list') }}";
     var getgradeBysubject = "{{ config('constants.api.get_grade_bysubject') }}";
+    var teacher_id = "{{ Session::get('ref_user_id') }}";
     // default image test
     var defaultImg = "{{ asset('public/images/users/default.jpg') }}";
 </script>
