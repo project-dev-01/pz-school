7c6ed9f4d4f979096eaa919865e93d6db143f2f0
kalai <sathya@aibots.my>
2023-05-25 17:06:14 +0530

    homeblade

--- resources/views/auth/home.blade.php
+++ resources/views/auth/home.blade.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8" />
-    <title>Home</title>
+    <title>Login</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" /> -->
     <meta name="description" content="Paxsuzen School is a premier educational institution that offers quality education to students of all ages. Our curriculum is designed to prepare future leaders for success in the global marketplace.">
@@ -18,153 +19,206 @@
     <link href="{{ asset('public/css/app.min.css') }}" rel="stylesheet" type="text/css" id="app-default-stylesheet" />
     <!-- icons -->
     <link href="{{ asset('public/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
-    <link href="{{ asset('public/css/custom/login.css') }}" rel="stylesheet" type="text/css" />
-    <link href="{{ asset('public/css/custom/opensans-font.css') }}" rel="stylesheet" type="text/css" />
-    <!-- <link href="{{ asset('public/css/custom-minified/admin_login.min.css') }}" rel="stylesheet" type="text/css" /> -->
+    <!--<link href="{{ asset('public/css/custom/admin_login.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('public/css/custom/opensans-font.css') }}" rel="stylesheet" type="text/css" />-->
+    <link href="{{ asset('public/css/custom-minified/admin_login.min.css') }}" rel="stylesheet" type="text/css" />
     <link href="{{ asset('public/css/custom-minified/opensans-font.min.css') }}" rel="stylesheet" type="text/css" />
-    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-    <link href="{{ asset('public/css/custom/homeresponsive.css') }}" rel="stylesheet" type="text/css" />
 </head>
+<style>
+    .smk 
+    {
+    height: 65px;
+    background: #D2EDEF;
+    font-family: 'Open Sans';
+    font-size: 14px;
+    text-align: center;
+    color: #16191D; width: 50%;
+    padding-left: 0px;
+    margin-left: 119px;
+    }
+    .map
+    {
+    width:100%;
+    height:100%;
+    }
+    .auth-user-testimonial
+    {
+    position: relative;
+    margin: 0 auto;
+    padding: 0 1.75rem;
+    bottom: 3rem;
+    left: 0;
+    right: 0;
+    }
+    .auth-fluid .auth-fluid-form-box 
+    {
+    max-width: 100%;
+    border-radius: 0;
+    z-index: 2;
+    background-color: #fff;
+    position: relative;
+    width: 100%;
+    height: auto;
+    }
+    @media screen and (min-device-width: 600px) and (max-device-width: 1024px) 
+    {
+    .auth-fluid .auth-fluid-right 
+    {
+        padding: 3rem 1rem;
+    }
+    .auth-fluid .auth-fluid-form-box
+    {
+    padding: 2rem 0rem;
+    }
+    }
+    @media screen and (min-device-width: 1920px) and (max-device-width: 1080px) 
+    {
+    .auth-fluid .auth-fluid-right 
+    {
+        padding: 5rem 1rem;
+    }
+    .auth-fluid .auth-fluid-form-box 
+    {
+    padding: 4rem 0rem;
+    }
+    }
+    body 
+    {
+    -ms-overflow-style: none; /* for Internet Explorer, Edge */
+    scrollbar-width: none; /* for Firefox */
+    overflow-y: scroll; 
+    }
+    body::-webkit-scrollbar 
+    {
+    display: none; /* for Chrome, Safari, and Opera */
+    }
+    </style>
 <body class="loading auth-fluid-pages pb-0">
-    <section class="features-list" >
-        <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
-            <div class="container-fluid">
-        <div class="row">
-            <div class="col-sm-6 col-md-6 col-lg-6 col-xl-6">
-                <div class="row">
-                    <div class="text-center">
-                        <div class="form-group mt-2">
-                        <div class="auth-brand text-center text-lg-left">
+        <div class="auth-fluid responsive" style="height:200px;">
+
+            <!-- Auth fluid right content -->
+            <div class="col-md-6 auth-fluid-right" style="background-color:#8b848414;color:black;">
+                <div class="auth-user-testimonial">
+                <div class="">
+                        <div class="form-group">
+                            <select class="vodiapicker">
+                                <option value="en" data-thumbnail="{{ config('constants.image_url').'/public/images/USA.png' }}">English</option>
+                                <option value="japanese" data-thumbnail="{{ config('constants.image_url').'/public/images/JPN.png' }}">日本語</option>
+                            </select>
+                            <div class="lang-select" style="float: right;">
+                                <button class="btn-select" value=""></button>
+                                <div class="b" style="text-align:justify;">
+                                    <ul id="a" style="margin-bottom:0px;"></ul>
+                                </div>
+                            </div>
+                        <!-- Logo -->
                             <div class="auth-logo">
                                 <div class="auth-logo">
                                     <a href="" class="logo logo-dark">
                                         <span class="logo-lg">
-                                            <img src="{{ config('constants.image_url').'/public/images/Suzen-app-logo.png' }}" alt="" height="60px" class="logo">
+                                            <img src="{{ config('constants.image_url').'/public/images/Suzen-app-logo.png' }}" alt="" height="50px">
                                         </span>
                                     </a>
                                 </div>
                             </div>
                         </div>
-                        <span class="badge badge-secondary smk text-center schl"><img src="{{ config('constants.image_url').'/public/images/'.$school_image }}" class="mr-2 rounded-circle" alt="">{{$school_name}}</span>
-                        </div>
-                        <div class="row hero-content">
-                            <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
-                                <h2 class="animated fadeInDown">Welcome To {{$school_name}} School</h2>
-                                <p class="color-white" style="margin-left: 30px; margin-right: 30px;margin-bottom: 3px;">To learn as much as I can, attain good grades and advance my education further.<br> I believe that self-motivation and a strict routine has helped me achieve my goals so far, and I will use the same method in the future.</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                
-                <div class="row">
-                    <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
-                        <h3 class="mb-3 text-center">Contact Us<span class="text-uppercase margin-l-20"></span></h3>
-                    </div>
-                </div>
-                <div class=" contact">
-                <div class="row">
-                    <div  class="col-sm-2 col-md-2 col-lg-2 col-xl-2"></div>
-                    <div class="col-sm-8 col-md-8 col-lg-8 col-xl-8 contactresponsive">
-                          <div class="row">
-                            <div  class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
-                            <div class="icon-item" style="margin-bottom: 5px;">
-                            <i class="fa fa-map-marker"></i>
-                            <span style="margin-left: 12px;">{{$home['address']}}</span>
-                        </div>
-                        </div>
-                        </div>
-                        <div class="row">
-                            <div  class="col-sm-8 col-md-8 col-lg-8 col-xl-8">
-                            <div class="icon-item" style="margin-bottom: 5px;">
-                            <i class="fa fa-phone"></i>
-                            <span style="margin-left: 10px;">{{$home['mobile_no']}}</span>
-                        </div>
-                        </div>
-                        </div>
-                        <div class="row">
-                            <div  class="col-sm-8 col-md-8 col-lg-8 col-xl-8">
-                            <div class="icon-item" style="margin-bottom: 5px;">
-                            <i class="fa fa-envelope"></i>
-                            <span style="margin-left: 7px;">{{$home['email']}}</span>
-                        </div>
-                        </div>
-                        </div>
-                        
-                    </div>
-                </div>
-                </div>
-                
-                <div class="row">
-                    <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
-                        <h3 class="text-center mb-3" style="margin-top: 10px;">Location<span class="text-uppercase margin-l-20"></span></h3>
+                        <!-- form -->
+                        <div class="form">
+                            <form id="LoginAuth" action="{{ route('admin.authenticate') }}" method="post">
+                                <div class="form-group">
+                                    <!-- <span class="badge badge-secondary smk"><img src="{{ asset('public/images/school.png') }}" class="mr-2 rounded-circle" alt="">SMK BERJAYA</span> -->
+                                    <span class="badge badge-secondary smk"><img src="{{ config('constants.image_url').'/public/images/'.$school_image }}" class="mr-2 rounded-circle" alt="">{{$school_name}}</span>
+                                    <!-- <span class="badge badge-secondary smk"><img src="{{ asset('public/images/school.jpg') }}" class="mr-2 rounded-circle" alt="">Maahad Tahfiz Al-Quran Darul Saadah Lilbanat</span> -->
+                                </div>
+
+                                <h3 class="animated fadeInDown" style="text-align: center;">Welcome To {{$school_name}} School</h3>
+                                <p class="card-text" style="text-align: center;">
+                                To learn as much as I can, attain good grades and advance my education further. 
+                                I believe that self-motivation and a strict routine has helped me achieve my goals so far,
+                                and I will use the same method in the future.</p>
+                                <h3 class="mb-2 text-center">Contact Us</h3>
+                                <div class="icon-item" style="margin-bottom: 5px; margin-left: 150px;">
+                                <i class="fa fa-map-marker"></i>
+                                <span style="margin-left: 12px;">{{$home['address']}}</span>
+                                </div>
+                                <div class="icon-item" style="margin-bottom: 5px; margin-left: 150px;">
+                                <i class="fa fa-phone"></i>
+                                <span style="margin-left: 10px;">{{$home['mobile_no']}}</span>
+                                </div>
+                                <div class="icon-item" style="margin-bottom: 5px; margin-left: 150px;">
+                                <i class="fa fa-envelope"></i>
+                                <span style="margin-left: 7px;">{{$home['email']}}</span>
+                                </div>
+                                <h3 class="text-center mb-2">Location</h3>
 						  <!-- Google Map -->
-						<div class="map">
-						    <iframe src="{{$home['location']}}" width="100%" height="210px" frameborder="0" style="border:0" allowfullscreen></iframe>
+                          
+						<div class="maps">
+						    <iframe src="{{$home['location']}}" frameborder="0" style="border:0;" allowfullscreen class="map"></iframe>
 						</div>
-                </div>
-               </div>
+                            </form>
+                        </div> 
+                            
+                    </div> <!-- end .align-items-center.d-flex.h-100-->
+                </div> <!-- end auth-user-testimonial-->
             </div>
             <!-- end Auth fluid right content -->
-
+            
             <!--Auth fluid left content -->
-            <div class="col-sm-6 col-md-6 col-lg-6 col-xl-6">
-                <div class="">
-                <div class="row">
-                    <!-- <div class="col-md-1 pt-2"></div> -->
-                    <div class="col-sm-6 col-md-6 col-lg-6 col-xl-6 pt-2">
-                                <!-- Simple card -->
+            <div class="col-md-6 auth-fluid-form-box" style="background-color:#8b848414;">
+                <div class="align-items-center d-flex h-100">
+                    <div class="card-body">
+                        <!-- Logo -->
+                       <div class="row">
+					   <div class="col-md-6" style="padding:10px">
                         <div class="card">
-                            <a href="{{ route('parent.login') }}">
-                                <img class="card-img-top img-fluid" src="{{$parent_image}}" alt="Card image cap">
-                                <div class="card-body">
-                                    <h5 class="card-title text-center">Parent Login</h5>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                    <div class="col-sm-6 col-md-6 col-lg-6 col-xl-6 pt-2">
-                                <!-- Simple card -->
-                        <div class="card">
-                            <a href="{{ route('student.login') }}">
-                                <img class="card-img-top img-fluid" src="{{$student_image}}" alt="Card image cap">
-                                <div class="card-body">
-                                    <h5 class="card-title text-center">Student Login</h5>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                    <div class="col-sm-6 col-md-6 col-lg-6 col-xl-6">
-                                <!-- Simple card -->
-                        <div class="card">
-                            <a href="{{ route('teacher.login') }}">
-                                <img class="card-img-top img-fluid" src="{{$teacher_image}}" alt="Card image cap">
-                                <div class="card-body">
-                                    <h5 class="card-title text-center">Teacher Login</h5>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                    <div class="col-sm-6 col-md-6 col-lg-6 col-xl-6">
-                                <!-- Simple card -->
-                        <div class="card">
-                            <a href="{{ route('schoolcrm.app.form') }}">
-                                <img class="card-img-top img-fluid" src="{{$application}}" alt="Card image cap">
-                                <div class="card-body">
-                                    <h5 class="card-title text-center">Application</h5>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                </div>
+                        <a href="{{ route('parent.login') }}">
+							<img src="{{$parent_image}}" style="height:200px" class="card-img-top" alt="...">
+							  <div class="card-body">
+                              <h6 class="card-title text-center" style="margin-bottom: 0px;">Parent Login</h6>
+								 </div>
+							</div>
+						</div>
+						<div class="col-md-6" style="padding:10px">
+						 <div class="card">
+                         <a href="{{ route('student.login') }}">
+							<img src="{{$student_image}}" style="height:200px" class="card-img-top" alt="...">
+							  <div class="card-body">
+							  <h6 class="card-title text-center"style="margin-bottom: 0px;">Teacher Login</h6>
+								 </div>
+								 </div>
+								 </div>
+						</div>
+						<div class="row">
+						<div class="col-md-6" style="padding:10px">
+						 <div class="card">
+                         <a href="{{ route('teacher.login') }}">
+							<img src="{{$teacher_image}}" style="height:200px" class="card-img-top" alt="...">
+							  <div class="card-body">
+							  <h6 class="card-title text-center"style="margin-bottom: 0px;">Teacher Login</h6>
+								 </div>
+						 </div>
+						 </div>
+						 <div class="col-md-6" style="padding:10px">
+						 <div class="card">                          
+                         <a href="{{ route('schoolcrm.app.form') }}">
+							<img src="{{$application}}" style="height:200px" class="card-img-top" alt="...">
+							  <div class="card-body">
+							  <h6 class="card-title text-center"style="margin-bottom: 0px;">Application</h6>
+								 </div>						  </div>
+								 </div>
+						</div>
+                        <!-- Footer-->
+                        <footer class="footer">
+                                <p class="text-muted">2020 - <script>
+                                        document.write(new Date().getFullYear())
+                                    </script> &copy; by <a href="javascript: void(0);" class="text-muted">Paxsuzen</a> </p>
+                            </footer>
+                    </div> <!-- end .card-body -->
+                </div> <!-- end .align-items-center.d-flex.h-100-->
             </div>
+            <!-- end auth-fluid-form-box-->
         </div>
-</div>
-</div>
-</div>
-    </section>
-    <!-- end auth-fluid-->
-
+        <!-- end auth-fluid-->
     <!-- Vendor js -->
     <script src="{{ asset('public/js/vendor.min.js') }}"></script>
     <script type="text/javascript">
@@ -219,29 +273,29 @@
             //console.log(value);
         });
 
-    $(".btn-select").click(function(){
-        $(".b").toggle();
-    });
-    //check local storage for the lang
-    var sessionLang = locale;
-    // console.log('en',sessionLang)
-    if (locale=="japanese"){
-        //find an item with value of sessionLang\
-        var img = "{{ config('constants.image_url').'/public/images/JPN.png' }}";
-        var value = "japanese";
-        var text = "JPN";
-        var item = '<li><img src="'+ img +'" alt="" /><span >'+ text +'</span></li>';
-        $('.btn-select').html(item);
-        $('.btn-select').attr('value', value);
-    } else {
-        var img = "{{ config('constants.image_url').'/public/images/USA.png' }}";
-        var value = "en";
-        var text = "ENG";
-        var item = '<li><img src="'+ img +'" alt="" /><span >'+ text +'</span></li>';
-        $('.btn-select').html(item);
-        $('.btn-select').attr('value', value);
-    }
-</script>
+        $(".btn-select").click(function() {
+            $(".b").toggle();
+        });
+        //check local storage for the lang
+        var sessionLang = locale;
+        // console.log('en',sessionLang)
+        if (locale == "japanese") {
+            //find an item with value of sessionLang\
+            var img = "{{ config('constants.image_url').'/public/images/JPN.png' }}";
+            var value = "japanese";
+            var text = "日本語";
+            var item = '<li><img src="' + img + '" alt="" /><span >' + text + '</span></li>';
+            $('.btn-select').html(item);
+            $('.btn-select').attr('value', value);
+        } else {
+            var img = "{{ config('constants.image_url').'/public/images/USA.png' }}";
+            var value = "en";
+            var text = "English";
+            var item = '<li><img src="' + img + '" alt="" /><span >' + text + '</span></li>';
+            $('.btn-select').html(item);
+            $('.btn-select').attr('value', value);
+        }
+    </script>
 
 </body>
 
